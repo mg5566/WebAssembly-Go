4f4f30ca32ecc33180a1b5b5329162970d61e3f6
Mike <mike.kangaroo.world@gmail.com>
2025-02-19 13:25:16 +0900

    [fix] change server port

--- server/server.go
+++ server/server.go
@@ -9,13 +9,13 @@ func main() {
 	log.Println("Server is starting...")
 
 	go func() {
-		err := http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+		err := http.ListenAndServe(":12345", http.FileServer(http.Dir(".")))
 		if err != nil {
 			log.Fatalf("Server failed to start: %v", err)
 		}
 	}()
 
-	log.Println("Server is running at http://localhost:8080")
+	log.Println("Server is running at http://localhost:12345")
 
 	// Keep the main function running
 	select {}
